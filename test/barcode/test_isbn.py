58e8f7af56b9652e38a08622316649cce615b4d7
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2019-05-22 16:18:28 +0900

    CC-260 fix indentation of test codes

--- test/barcode/test_isbn.py
+++ test/barcode/test_isbn.py
@@ -20,17 +20,16 @@ class TestIsbn(unittest.TestCase):
         # self.assertEqual(4, convert_isbn_to_asin('491234567890'))
         # self.assertEqual(2, convert_isbn_to_asin('351386327796'))
 
+    def test_generate_isbn_10_check_digit(self):
+        from nzigen_low_level.barcode.isbn import generate_isbn_10_check_digit
+        self.assertEqual('', generate_isbn_10_check_digit('40628203'))
+        self.assertEqual('2', generate_isbn_10_check_digit('406282038'))
+        self.assertEqual('8', generate_isbn_10_check_digit('488552090'))
+        self.assertEqual('X', generate_isbn_10_check_digit('462781741'))
 
-def test_generate_isbn_10_check_digit(self):
-    from nzigen_low_level.barcode.isbn import generate_isbn_10_check_digit
-    self.assertEqual('', generate_isbn_10_check_digit('40628203'))
-    self.assertEqual('2', generate_isbn_10_check_digit('406282038'))
-    self.assertEqual('8', generate_isbn_10_check_digit('488552090'))
-    self.assertEqual('X', generate_isbn_10_check_digit('462781741'))
 
-
-def test_generate_isbn_13_check_digit(self):
-    from nzigen_low_level.barcode.isbn import generate_isbn_13_check_digit
-    self.assertEqual('', generate_isbn_13_check_digit('97848855209'))
-    self.assertEqual('7', generate_isbn_13_check_digit('978488552090'))
-    self.assertEqual('8', generate_isbn_13_check_digit('978-462781741'))
+    def test_generate_isbn_13_check_digit(self):
+        from nzigen_low_level.barcode.isbn import generate_isbn_13_check_digit
+        self.assertEqual('', generate_isbn_13_check_digit('97848855209'))
+        self.assertEqual('7', generate_isbn_13_check_digit('978488552090'))
+        self.assertEqual('8', generate_isbn_13_check_digit('978-462781741'))
